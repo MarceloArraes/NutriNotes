50e4ee1a063e2a6d4141c23c61249dcee0eae603
marcelo <marcelo.arraes@gmail.com>
2022-03-01 15:45:22 +0000

    improv: shortened estimativa de peso

--- components/estimativaDePeso.js
+++ components/estimativaDePeso.js
@@ -24,12 +24,12 @@ export default function EstimativaDePeso(props){
           {/* <MathJax>{`\\(IMC=\\frac{peso}{altura^2}=\\frac{${props.peso}}{${(props.altura*props.altura).toFixed(2)}} \\approx {${(props.imc).toFixed(2)}}\\)`}</MathJax> */}
           {props.sexo=='homem'?
           <>
-          <MathJax>{`\\(Peso (kg) = 1.73  \\times  CB + 0.98 \\times CP + 0.37 \\times DCSE + 1.16 \\times AJ– 81.69\\)`}</MathJax>
-          <MathJax>{`\\(Peso (kg) = 1.73  \\times  {${props.CB}} + 0.98 \\times {${props.CP}} + 0.37 \\times {${props.DCSE}} + 1.16 \\times {${props.AJ}} – 81.69={${props.estimativaDePeso.toFixed(2)}}\\)`}</MathJax>
+          <MathJax>{`\\(Peso = 1.73  \\times  CB + 0.98 \\times CP + 0.37 \\times DCSE + 1.16 \\times AJ– 81.69\\)`}</MathJax>
+          <MathJax>{`\\(Peso = 1.73  \\times  {${props.CB}} + 0.98 \\times {${props.CP}} + 0.37 \\times {${props.DCSE}} + 1.16 \\times {${props.AJ}} – 81.69={${props.estimativaDePeso.toFixed(2)}}\\)`}</MathJax>
           </>:
           <>
-          <MathJax>{`\\(Peso (kg) = 0.98  \\times  CB + 1.27 \\times CP + 0.4 \\times DCSE + 0.87 \\times AJ – 62.35\\)`}</MathJax>
-          <MathJax>{`\\(Peso (kg) = 0.98  \\times  {${props.CB}} + 1.27 \\times {${props.CP}} + 0.4 \\times {${props.DCSE}} + 0.87 \\times {${props.AJ}} –  62.35={${props.estimativaDePeso.toFixed(2)}}\\)`}</MathJax>
+          <MathJax>{`\\(Peso = 0.98  \\times  CB + 1.27 \\times CP + 0.4 \\times DCSE + 0.87 \\times AJ – 62.35\\)`}</MathJax>
+          <MathJax>{`\\(Peso = 0.98  \\times  {${props.CB}} + 1.27 \\times {${props.CP}} + 0.4 \\times {${props.DCSE}} + 0.87 \\times {${props.AJ}} –  62.35={${props.estimativaDePeso.toFixed(2)}}\\)`}</MathJax>
           </>}
           </Text>
           </Box>
