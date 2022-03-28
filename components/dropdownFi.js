401f4c5f8c7a59bf4eebc272f14de2792fa490d8
marcelo <marcelo.arraes@gmail.com>
2022-03-28 02:38:54 +0000

    feat: checkboxes working, parent subscribes children and vice versa.

--- components/dropdownFi.js
+++ components/dropdownFi.js
@@ -11,7 +11,18 @@ import Typography from '@mui/material/Typography';
 
 export default function  DropdownFi(props) {
   const [openAmputacao, setOpenAmputacao] = useState(false);
-  const [checked, setChecked] = React.useState([true, false]);
+  const [checked, setChecked] = useState([true, false]);
+
+  //One way of doing it is to create a useState for every body part, like:
+  //const [checkedBracoEsquerdo, setCheckedBracoEsquerdo] = React.useState([false, false, false]);
+  //const [checkedBracoDireito, setCheckedBracoDireito] = React.useState([false, false, false]);
+  //const [checkedPernaEsquerda, setCheckedPernaEsquerda] = React.useState([false, false, false]);
+  //const [checkedPernaDireita, setCheckedPernaDireita] = React.useState([false, false, false]);
+  //Here the parent is just the result of their childrens. 
+  //Or i can try to create a matrix of the body parts, like:
+  //const [bodyPartsList, setbodyPartsList] = React.useState([[false, false, false]]);
+  //
+  
 
   useEffect(() => {
     if(props.condicaoClinica!='' && props.level!=''){
@@ -56,73 +67,64 @@ export default function  DropdownFi(props) {
   ],
   }
 
-  const objBodyParts = {
+  const [objBodyParts, setObjBodyParts] = useState({
     obs:[
-    {"id":1,"name":"Braço Esquerdo", "value": "false", "children1": [
-      {"id":2,"name":"Ombro Esquerdo", "value": false},
-      {"id":3,"name":"Antebraço Esquerdo", "value": false},
-      {"id":4,"name":"Mão Esquerda", "value": false},
+    {"id":"1","name":"Braço Esquerdo", "value": false, "children1": [
+      {"id":"2","name":"Ombro Esquerdo", "value": false},
+      {"id":"3","name":"Antebraço Esquerdo", "value": false},
+      {"id":"4","name":"Mão Esquerda", "value": false},
     ]
     },
-    {"id":5,"name":"Braço Direito", "value": false, "children1": [
-      {"id":6,"name":"Ombro Direito", "value": false},
-      {"id":7,"name":"Antebraço Direito", "value": false},
-      {"id":8,"name":"Mão Direita", "value": false},
+    {"id":"5","name":"Braço Direito", "value": false, "children1": [
+      {"id":"6","name":"Ombro Direito", "value": false},
+      {"id":"7","name":"Antebraço Direito", "value": false},
+      {"id":"8","name":"Mão Direita", "value": false},
     ],
     },
-    {"id":9,"name":"Perna Esquerda", "value": "false", "children1": [
-      {"id":10,"name":"Coxa Esquerda", "value": false},
-      {"id":11,"name":"Canela Esquerda", "value": false},
-      {"id":12,"name":"Pé Esquerdo", "value": false},
+    {"id":"9","name":"Perna Esquerda", "value": false, "children1": [
+      {"id":"10","name":"Coxa Esquerda", "value": false},
+      {"id":"11","name":"Canela Esquerda", "value": false},
+      {"id":"12","name":"Pé Esquerdo", "value": false},
     ]
     },
-    {"id":13,"name":"Perna Direita", "value": "false", "children1": [
-      {"id":14,"name":"Coxa Direita", "value": false},
-      {"id":15,"name":"Canela Direita", "value": false},
-      {"id":16,"name":"Pé Direito", "value": false},
+    {"id":"13","name":"Perna Direita", "value": false, "children1": [
+      {"id":"14","name":"Coxa Direita", "value": false},
+      {"id":"15","name":"Canela Direita", "value": false},
+      {"id":"16","name":"Pé Direito", "value": false},
     ]
     },
   ]
+  })
+
+  const handleChange4 = (event) => {
+    console.log(event.target.value);
+    
+    let copyOfObject = { ...objBodyParts }
+
+    if(event.target.name==="parent"){
+      console.log(event.target.name);
+      console.log(event.target.id);
+      console.log(event.target.checked);
+      copyOfObject.obs.map(item => {item.id===event.target.id? item.children1.map(item2 => {  item2.value = event.target.checked  }) : null})
+      setObjBodyParts(copyOfObject);
+
+    }else{
+    copyOfObject.obs.map(item => { item.children1.map(item2 => { item2.id === event.target.id ? item2.value = event.target.checked : null }) })
+    setObjBodyParts(copyOfObject);
   }
 
-  //Começa CheckBox Amputaçao
-  const handleChange1 = (event) => {
-    setChecked([event.target.checked, event.target.checked]);
-  };
-
-  const handleChange2 = (event) => {
-    setChecked([event.target.checked, checked[1]]);
-  };
-
-  const handleChange3 = (event) => {
-    setChecked([checked[0], event.target.checked]);
-  };
-
-  const children = (
-    <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
-      <FormControlLabel
-        label="Antebraço"
-        control={<Checkbox checked={checked[0]} onChange={handleChange2} />}
-      />
-      <FormControlLabel
-        label="Mão"
-        control={<Checkbox checked={checked[1]} onChange={handleChange3} />}
-      />
-    </Box>
-  );
+  }
 
     function ListChildren(props){
       const parent = props.bodyParts;
       return (
       <Box sx={{ display: 'flex', flexDirection: 'column', ml: 3 }}>
       {parent.children1.map((item, index) => {
-        console.log(item);
-        console.log(item.name);
         return (
       <FormControlLabel
         key={item.id}
         label={item.name}
-        control={<Checkbox checked={item.value} onChange={handleChange2} />}
+        control={<Checkbox id={item.id} checked={item.value} onChange={handleChange4} />}
       /> 
         )
       })}
@@ -187,18 +189,17 @@ export default function  DropdownFi(props) {
             label={bodyParts.name}
             control={
               <Checkbox
-                checked={checked[0] && checked[1]}
-                indeterminate={checked[0] !== checked[1]}
-                onChange={handleChange1}
+                name="parent"
+                id={bodyParts.id}
+                checked={bodyParts.children1[0].value && bodyParts.children1[1].value && bodyParts.children1[2].value}
+                indeterminate={bodyParts.children1[0].value !== bodyParts.children1[1].value || bodyParts.children1[0].value !== bodyParts.children1[2].value}
+                onChange={handleChange4}
               />
             }
           />
           <ListChildren bodyParts={bodyParts}/>
           </div>
         ))}
-
-
-        
         
         </Box>}
     </Box>
